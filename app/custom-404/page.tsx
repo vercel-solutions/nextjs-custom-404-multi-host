82d38d2d4a8c7885c5ae3867342186b13089b785
Luis Alvarez D <luis@vercel.com>
2025-03-10 23:18:01 -0500

    Removed previous implementation

--- app/custom-404/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8 gap-8 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-4xl font-bold">404 (custom)</h1>
-      <p className="text-lg">This page could not be found.</p>
-    </div>
-  );
-}
