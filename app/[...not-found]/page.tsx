31071ae8ae735cb952f1388c25a0742832e50f89
Luis Alvarez D <luis@vercel.com>
2025-03-11 09:01:44 -0500

    Use the root not-found

--- app/[...not-found]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { notFound } from "next/navigation";
-
-export default function NotFoundCatchAll() {
-	// Call the notFound function to trigger the local not-found page.
-	notFound();
-}
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,7 +7,7 @@ export default async function NotFound() {
 
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen p-8 gap-8 font-[family-name:var(--font-geist-sans)]">
-			<h1 className="text-4xl font-bold">Custom 404</h1>
+			<h1 className="text-4xl font-bold">404</h1>
 			<p className="text-lg">This page could not be found.</p>
 			<p className="text-lg">Host: {host}</p>
 		</div>
